d62fc2b8ceadd78cf8d3d2d4efa0d378facf08ae
Felipe Cys Laskoski <lipecys@gmail.com>
2011-06-06 13:49:15 -0300

    adicionado leitura e escreita de buffer no cliente.c e servidor.c

--- src/client.c
+++ src/client.c
@@ -2,7 +2,7 @@
 
 /* Função do Cliente - Utilizado para retirar mensagens da rede e inseri-las no buffer */
 void * cliente (info_pkg *config_node) {
-    client_server_info * client;
+/*    client_server_info * client;
 
     client = config_client(config_node->port, config_node->ip_address);
 
@@ -14,6 +14,36 @@ void * cliente (info_pkg *config_node) {
         //}
         //buf[numbytes] = '\0';
         //printf("client: received '%s'\n",buf);
-    }
+*/
+
+
+        do {
+            // Início região crítica
+            sem_wait(&config_node->threads.full);
+            pthread_mutex_lock(&config_node->threads.mutex);
+
+
+            // consome item da fila
+            if (config_node->threads.mensagens->tamanho == 5)
+            {
+                while(!vazia_lista(config_node->threads.mensagens))
+                {
+                    /* <<FELIPE>> envia mensagem*/
+                    printf("%s\n", (char *)remove_lista(config_node->threads.mensagens));
+                }
+            }
+
+
+            pthread_mutex_unlock(&config_node->threads.mutex);
+            sem_post(&config_node->threads.empty);
+            // Fim região crítica
+        } while(CONTINUA);
+
+
+        pthread_exit((void*)1);
+
+
+/*    }
     close(client->sockfd);
+*/
 }
--- src/server.c
+++ src/server.c
@@ -7,7 +7,7 @@ void * servidor (info_pkg *config_node) {
     socklen_t clilen;
     int newconn;
     
-    server = config_server(config_node->port);
+/*    server = config_server(config_node->port);
     clilen = sizeof(cliaddr);
     newconn = accept(server->sockfd, (struct sockaddr *)&cliaddr, &clilen);
     
@@ -26,7 +26,73 @@ void * servidor (info_pkg *config_node) {
             //puts("Entre com a string");
             //gets(str);
             //puts(str);
-        }
+*/
+
+
+            do {
+                int tam_buffer = 256;
+                char *buffer = (char *) malloc(tam_buffer * sizeof(char));
+                int cont_caracteres = 0;
+                char *string_final;
+                char destino;
+
+                printf("Especifique o destino (1 a 4):\n");
+                destino = getchar();
+
+                getchar();
+
+                printf("Digite a mensagem:\n");
+
+
+                buffer[cont_caracteres] = getchar();
+                while (buffer[cont_caracteres] != EOL)
+                {
+                    cont_caracteres++;
+                    if ( cont_caracteres == tam_buffer )
+                    {
+                        tam_buffer *= 2;
+                        buffer = (char *) realloc(buffer, tam_buffer * sizeof(char));
+                    }
+                    buffer[cont_caracteres] = getchar();
+                }
+
+                /*
+                * criando uma string do tamanho da entrada lida
+                * O + 4 é porque precisamos dos seguintes bits de controle:
+                * - destino
+                * - origem
+                * - aceitação 
+                * - '\0' da string
+                */
+                string_final = (char *) malloc( (cont_caracteres + 4)* sizeof(char));
+                memcpy(string_final+3, buffer, cont_caracteres);
+                string_final[0] = destino;
+                string_final[1] = config_node->computer_number;
+                string_final[2] = '0';
+                string_final[cont_caracteres+3] = '\0';
+
+                // Início região crítica
+                sem_wait(&config_node->threads.empty);
+                pthread_mutex_lock(&config_node->threads.mutex);
+
+
+                // inserir novo item na fila
+                insere_lista_fim((objeto)string_final, config_node->threads.mensagens);
+
+
+                pthread_mutex_unlock(&config_node->threads.mutex);
+                sem_post(&config_node->threads.full);
+                // Fim região crítica
+
+
+                free(buffer);
+            } while(CONTINUA);
+
+            pthread_exit((void*)0);
+
+
+/*        }
         close(server->sockfd);
     }
+*/
 }
